d9013f8c1dcbdb5926d4a521433c6f925e37a79e
Teguh Ikhlas <teguh4work@gmail.com>
2024-05-23 11:50:31 +0700

    menambahkan api login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
 
 class LoginController extends Controller
 {
@@ -19,11 +21,19 @@ class LoginController extends Controller
             'password' => 'required',
         ]);
 
-        // Logika login sederhana (tanpa database)
-        if ($request->email === 'admin@example.com' && $request->password === 'password') {
+         // Mengirim permintaan ke API untuk memverifikasi kredensial
+         $response = Http::post('http://localhost:8001/api/login', [
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        // Memeriksa respons dari API
+        if ($response->successful()) {
+            // Login berhasil, alihkan ke halaman home
             return redirect()->route('home');
         }
 
+        // Login gagal, kembali ke halaman login dengan pesan error
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ]);
